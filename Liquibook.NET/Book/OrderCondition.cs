8077d4e29aaf0326f6ba0d9bf9c577d4b5971cec
jkulubya <jameskitamirike@gmail.com>
2018-04-03 15:44:20 +0300

    Order conditions

---
+++ Liquibook.NET/Book/OrderCondition.cs
@@ -0,0 +1,11 @@
+﻿namespace Liquibook.NET.Book
+{
+    public enum OrderCondition
+    {
+        NoConditions = 0,
+        AllOrNone = 1,
+        ImmediateOrCancel = 2,
+        FillOrKill = 3,
+        Stop = 4
+    }
+}
