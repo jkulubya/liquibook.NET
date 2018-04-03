f6e14b9fc79e68dcf622bb1cbef3ab3fcfa9dfbe
jkulubya <jameskitamirike@gmail.com>
2018-04-03 15:44:28 +0300

    IOrder

---
+++ Liquibook.NET/Book/IOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace Liquibook.NET.Book
+{
+    public interface IOrder
+    {
+        bool IsLimit { get; set; }
+        bool IsBuy { get; set; }
+        int Price { get; set; }
+        int StopPrice { get; set; }
+        int OrderQty { get; set; }
+        bool AllOrNone { get; set; }
+        bool ImmediateOrCancel { get; set; }
+        
+        // TODO: investigate methods at bottom of related cpp class
+    }
+}
