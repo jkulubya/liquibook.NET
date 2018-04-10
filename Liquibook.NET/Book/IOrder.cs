ee7053bf13258325bd6cf1d60adc930a5a43be2d
jkulubya <jameskitamirike@gmail.com>
2018-04-10 09:32:47 +0300

    Change iorder to use quantity and price types

--- Liquibook.NET/Book/IOrder.cs
+++ Liquibook.NET/Book/IOrder.cs
@@ -1,12 +1,14 @@
-﻿namespace Liquibook.NET.Book
+﻿using Liquibook.NET.Types;
+
+namespace Liquibook.NET.Book
 {
     public interface IOrder
     {
         bool IsLimit { get; set; }
         bool IsBuy { get; set; }
-        int Price { get; set; }
-        int StopPrice { get; set; }
-        int OrderQty { get; set; }
+        Price Price { get; set; }
+        Price StopPrice { get; set; }
+        Quantity OrderQty { get; set; }
         bool AllOrNone { get; set; }
         bool ImmediateOrCancel { get; set; }
         
