cd5197102d52682fb1458f8f5c4722c719dac425
jkulubya <jameskitamirike@gmail.com>
2018-04-19 10:47:35 +0300

    Change int constants to Price struct

--- Liquibook.NET/Book/DepthConstants.cs
+++ Liquibook.NET/Book/DepthConstants.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Liquibook.NET.Types;
 
 namespace Liquibook.NET.Book
 {
     public static class DepthConstants
     {
-        public static int InvalidLevelPrice = 0;
-        public static int MarketOrderBidSortPrice = int.MaxValue;
-        public static int MarketOrderAskSortPrice = 0;
+        public static Price InvalidLevelPrice = 0;
+        public static Price MarketOrderBidSortPrice = int.MaxValue;
+        public static Price MarketOrderAskSortPrice = 0;
     }
 }
