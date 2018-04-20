cee72b343e55e9aabab88ee2483c66ef1a20a500
jkulubya <jameskitamirike@gmail.com>
2018-04-20 10:07:41 +0300

    Mess around with some perf stuff

---
+++ Perf/OrderBookBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Liquibook.NET.Book;
+
+namespace Perf
+{
+    [MemoryDiagnoser]
+    public class OrderBookBenchmark
+    {
+        private OrderBook Book { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Book = new OrderBook();
+        }
+
+        [Benchmark]
+        public void EnterInBook()
+        {
+            var order = new Order()
+            {
+                IsBuy = true,
+                OrderQty = 100,
+                Price = 100000
+            };
+            Book.Add(order);
+        }
+        
+        [Benchmark]
+        public void EnterThenCancelInBook()
+        {
+            var order = new Order()
+            {
+                IsBuy = true,
+                OrderQty = 100,
+                Price = 100000
+            };
+            Book.Add(order);
+            Book.Cancel(order);
+        }
+    }
+}
---
+++ Perf/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Perf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<OrderBookBenchmark>();
+        }
+    }
+}
